124d784d509196460ebbea1835ebfb7700ac4330
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-12-03 14:29:44 +0100

    make handle_get_printable_amount return an error

--- src/swap/handle_get_printable_amount.c
+++ src/swap/handle_get_printable_amount.c
@@ -5,16 +5,24 @@
 #include <stdint.h>
 
 void handle_get_printable_amount(get_printable_amount_parameters_t* params) {
+    params->result = 0;
     params->printable_amount[0] = 0;
     if (params->amount_length > 8) {
         PRINTF("Amount is too big");
         return;
     }
-    unsigned char amount_buffer[8];
-    os_memset(amount_buffer, 0, 8);
-    os_memcpy(amount_buffer + (8 - params->amount_length), params->amount, params->amount_length);
-    uint64_t amount = readUnsigned64(amount_buffer);
+
+    unsigned char buffer[8];
+    os_memset(buffer, 0, sizeof(buffer));
+    os_memcpy(buffer + sizeof(buffer) - params->amount_length,
+              params->amount,
+              params->amount_length);
+
+    uint64_t amount = readUnsigned64(buffer);
     if (xrp_print_amount(amount, params->printable_amount, sizeof(params->printable_amount)) != 0) {
-        THROW(EXCEPTION);
+        PRINTF("xrp_print_amount failed");
+        return;
     }
+
+    params->result = 1;
 }
