835f00798ac41f5e8640c40faac927f355c14331
greenknot <greenknot@users.noreply.github.com>
2020-12-03 14:04:43 +0100

    fix handle_check_address if get_publicKey fails

--- src/swap/handle_check_address.c
+++ src/swap/handle_check_address.c
@@ -23,17 +23,18 @@ void handle_check_address(check_address_parameters_t* params) {
     cx_ecfp_public_key_t public_key;
     int error = get_publicKey(CX_CURVE_256K1, bip32_path_ptr, bip32PathLength, &public_key, NULL);
     if (error) {
-        THROW(error);
+        PRINTF("get_publicKey failed\n");
+        return;
     }
 
     char address[41];
     get_address(&public_key, address, sizeof(address));
 
     if (os_strcmp(address, params->address_to_check) != 0) {
-        // os_memcpy(params->address_to_check, address, strlen(address));
         PRINTF("Addresses don't match\n");
         return;
     }
-    PRINTF("Addresses  match\n");
+
+    PRINTF("Addresses match\n");
     params->result = 1;
 }
