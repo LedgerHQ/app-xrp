4056147995a97fcb77b58670c6fbb55167a10889
greenknot <greenknot@users.noreply.github.com>
2020-12-04 12:24:18 +0100

    fix lint in handle_swap_sign_transaction.c

--- src/swap/handle_swap_sign_transaction.c
+++ src/swap/handle_swap_sign_transaction.c
@@ -8,18 +8,21 @@ bool copy_transaction_parameters(create_transaction_parameters_t* params) {
     swapStrings_t stack_data;
     memset(&stack_data, 0, sizeof(stack_data));
     strncpy(stack_data.address, params->destination_address, sizeof(stack_data.address) - 1);
-    strncpy(stack_data.destination_tag, params->destination_address_extra_id, sizeof(stack_data.destination_tag));
+    strncpy(stack_data.destination_tag,
+            params->destination_address_extra_id,
+            sizeof(stack_data.destination_tag));
     if ((stack_data.address[sizeof(stack_data.address) - 1] != '\0') ||
         (stack_data.destination_tag[sizeof(stack_data.destination_tag) - 1] != '\0') ||
-        (params->amount_length > 8) ||
-        (params->fee_amount_length > 8)) {
+        (params->amount_length > 8) || (params->fee_amount_length > 8)) {
         return false;
     }
     // store amount as big endian in 8 bytes, so the passed data should be alligned to right
     // input {0xEE, 0x00, 0xFF} should be stored like {0x00, 0x00, 0x00, 0x00, 0x00, 0xEE, 0x00,
     // 0xFF}
     memcpy(stack_data.amount + 8 - params->amount_length, params->amount, params->amount_length);
-    memcpy(stack_data.fees + 8 - params->fee_amount_length, params->fee_amount, params->fee_amount_length);
+    memcpy(stack_data.fees + 8 - params->fee_amount_length,
+           params->fee_amount,
+           params->fee_amount_length);
     memcpy(&approvalStrings.swap, &stack_data, sizeof(stack_data));
 
     return true;
