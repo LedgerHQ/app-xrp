bc8e436cc0d27cf2d7a144c924f1b56799062f48
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-01-06 12:35:00 +0100

    tmp undoing change breaking swap retro-compatibility

--- src/main.c
+++ src/main.c
@@ -279,10 +279,11 @@ static void library_main_helper(struct libargs_s *args) {
             }
             break;
         case GET_PRINTABLE_AMOUNT:
-            // ensure result is zero if an exception is thrown
-            args->get_printable_amount->result = 0;
-            args->get_printable_amount->result =
-                handle_get_printable_amount(args->get_printable_amount);
+            // ensure result is zero if an exception is thrown (compatibility breaking, disabled
+            // until LL is ready)
+            // args->get_printable_amount->result = 0;
+            // args->get_printable_amount->result =
+            handle_get_printable_amount(args->get_printable_amount);
             break;
         default:
             break;
