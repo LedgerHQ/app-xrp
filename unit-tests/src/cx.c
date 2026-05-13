8a8839e3d37d663b637383b617ff03c39a045bfc
Alexis Grojean <alexis.grojean@ledger.fr>
2026-05-13 14:35:40 +0200

    Add OpenSSL 1.1/3.0 portability guards in unit-tests/src/cx.c.

--- unit-tests/src/cx.c
+++ unit-tests/src/cx.c
@@ -5,10 +5,13 @@
 #include <stdbool.h>
 
 #include <cmocka.h>
+#include <openssl/opensslv.h>
 #include <openssl/sha.h>
 #include <openssl/ripemd.h>
 #include <openssl/evp.h>
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
 #include <openssl/provider.h>
+#endif
 
 #include "cx.h"
 
@@ -34,12 +37,16 @@ int cx_hash_no_throw(cx_hash_t *hash,
                      size_t out_len) {
     uint32_t digSize = 0;
     EVP_MD_CTX *md_ctx = EVP_MD_CTX_new();
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     OSSL_PROVIDER *prov = NULL;
+#endif
 
     if (hash->algo == CX_SHA256) {
         EVP_DigestInit(md_ctx, EVP_sha256());
     } else if (hash->algo == CX_RIPEMD160) {
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
         prov = OSSL_PROVIDER_load(NULL, "legacy");
+#endif
         EVP_DigestInit(md_ctx, EVP_ripemd160());
     } else {
         assert_true(false);
@@ -48,7 +55,9 @@ int cx_hash_no_throw(cx_hash_t *hash,
     EVP_DigestUpdate(md_ctx, (const void *) in, len);
     EVP_DigestFinal(md_ctx, out, (unsigned int *) &digSize);
     EVP_MD_CTX_free(md_ctx);
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     if (prov) OSSL_PROVIDER_unload(prov);
+#endif
 
     if (digSize != out_len) {
         assert_true(false);
